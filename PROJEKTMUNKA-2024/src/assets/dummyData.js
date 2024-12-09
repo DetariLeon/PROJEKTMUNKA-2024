0867e515fb4d48e28ded6f39c37f4633ab4db046
viktorkaa <83224403+viktorkaa@users.noreply.github.com>
2024-12-09 13:45:02 +0100

    dummyData.js
    +2 étel hozzádasa

---
+++ PROJEKTMUNKA-2024/src/assets/dummyData.js
@@ -0,0 +1,45 @@
+export const recipes = [
+    {
+        id: 1,
+        name: 'Rantot',
+        cookTime: '45',
+        difficulty: 'könnyű',
+        imageUrl: 'https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+        id: 2,
+        name: 'Paprikás Csirke',
+        cookTime: '60',
+        difficulty: 'közepes',
+        imageUrl: 'https://images.pexels.com/photos/600491/pexels-photo-600491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+        id: 3,
+        name: 'Lecsó',
+        cookTime: '30',
+        difficulty: 'könnyű',
+        imageUrl: 'https://images.pexels.com/photos/775328/pexels-photo-775328.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+        id: 4,
+        name: 'Töltött Káposzta',
+        cookTime: '120',
+        difficulty: 'nehéz',
+        imageUrl: 'https://images.pexels.com/photos/6287514/pexels-photo-6287514.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+        id: 5,
+        name: 'Somlói Galuska',
+        cookTime: '90',
+        difficulty: 'közepes',
+        imageUrl: 'https://images.pexels.com/photos/6607400/pexels-photo-6607400.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+        id: 6,
+        name: 'Gulyásleves',
+        cookTime: '75',
+        difficulty: 'közepes',
+        imageUrl: 'https://images.pexels.com/photos/675951/pexels-photo-675951.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    }
+];
+
